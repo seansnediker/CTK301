90363c42d50efd2edec5d03a5fecc395b149c0e5
seansnediker <94629278+seansnediker@users.noreply.github.com>
2022-04-25 17:54:11 -0500

    Delete FinalStatus directory

--- FinalStatus/mosaicscript.js
+++
@@ -1,9 +0,0 @@
-$( "#gallery .item" ).hover(
-  function() {
-    $( '#gallery' ).addClass( "hover" );
-    $( this ).addClass( "hover" );
-  }, function() {
-    $( this ).removeClass( "hover" );
-    $( '#gallery' ).removeClass( "hover" );
-  }
-);
